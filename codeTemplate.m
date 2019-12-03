ebb03b41ed8649f4533bb50e3fe8f60c6bd583e0
AnmolPardeshi <anmolanp@usc.edu>
2019-12-02 18:12:56 -0800

    basic template for codes added. Allows you to generate a diary to maintain records & assocaited minutiea

---
+++ codeTemplate.m
@@ -0,0 +1,20 @@
+%% starting descriptive
+diary %name.txt; diary on;
+disp('Descriptive:');
+disp('input -> ');
+disp('procedure -> ');
+diary off;
+%% main code 
+clear all
+close all
+clc
+
+%% save main variables
+
+%% ending decriptive
+clc; diary on;
+disp('output -> main varibles -> ');
+disp('--------------------------------------');disp('Stats :');
+
+disp('__________________________________________________________________________________________________');
+diary off;
